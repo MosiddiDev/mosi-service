b8f1b46a94824f91507b684d869c77bece903ffa
Mohammed Siddeeq <msiddeeq15@gmail.com>
2015-02-25 15:23:10 -0500

    config service..

--- config.py
+++ config.py
@@ -12,13 +12,14 @@
 
 # 	return send_from_directory('configs/{app}/{env}'.format(app=application, env=environment.split('.')[0]), 'private.json', as_attachment=True) if '.json' in environment else open('configs/{app}/{env}/private.json'.format(app=application, env=environment)).read()
 
+from flask import Response
 from web import app
 
 
 @app.route("/config/<path:application>/<path:environment>", methods=['GET'])
 def public_config(application, environment):
-	return open('configs/{appname}/{env}/public.json'.format(appname=application, env=environment)).read()
+	return Response(open('configs/{appname}/{env}/public.json'.format(appname=application, env=environment)).read(), headers={'content-type': 'application/json'})
 
 @app.route("/config/private/<path:application>/<path:environment>", methods=['GET'])
 def private_config(application, environment):
-	return open('configs/{appname}/{env}/private.json'.format(appname=application, env=environment)).read()
+	return Response(open('configs/{appname}/{env}/private.json'.format(appname=application, env=environment)).read(), headers={'content-type': 'application/json'})
