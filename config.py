949394b25afb49bac87923d0adbe66dda08ec782
Mohammed Siddeeq <msiddeeq15@gmail.com>
2015-02-25 12:30:52 -0500

    config service..

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ def test_js():
 	# return jsonify(response_object)
 	return send_from_directory('configs/subscribe/stage', 'public.json', as_attachment=True)
 
-@app.route("/config/<path:environment>/<path:application>", methods=['GET'])
+@app.route("/config/<path:application>/<path:environment>", methods=['GET'])
 def public_config(environment, application):
 
-	return send_from_directory('configs/{app}/{env}', 'public.json', as_attachment=True) if '.json' in application else open('configs/{app}/{env}/public.json'.format(env=environment, app=application)).read()
+	return send_from_directory('configs/{app}/{env}'.format(env=environment, app=application.split('.')[0]), 'public.json', as_attachment=True) if '.json' in application else open('configs/{app}/{env}/public.json'.format(env=environment, app=application)).read()
 
-@app.route("/config/private/<path:environment>/<path:application>", methods=['GET'])
+@app.route("/config/private/<path:application>/<path:environment>", methods=['GET'])
 def private_config(environment, application):
 
-	return send_from_directory('configs/{app}/{env}', 'private.json', as_attachment=True) if '.json' in application else open('configs/{app}/{env}/private.json'.format(env=environment, app=application)).read()
+	return send_from_directory('configs/{app}/{env}'.format(env=environment, app=application.split('.')[0]), 'private.json', as_attachment=True) if '.json' in application else open('configs/{app}/{env}/private.json'.format(env=environment, app=application)).read()
