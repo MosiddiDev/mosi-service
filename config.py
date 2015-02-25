8a70a3e28b6b3802804de253af9b743d8e753c30
Mohammed Siddeeq <msiddeeq15@gmail.com>
2015-02-25 15:02:26 -0500

    config service..

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-from flask import request, jsonify, Response, render_template, send_from_directory
-from web import app
-import requests
-import time
-import json
-import urllib2
+# from flask import send_from_directory
+# from web import app
 
 
 # @app.route("/config/<path:application>/<path:environment>", methods=['GET'])
@@ -16,14 +12,13 @@ import urllib2
 
 # 	return send_from_directory('configs/{app}/{env}'.format(app=application, env=environment.split('.')[0]), 'private.json', as_attachment=True) if '.json' in environment else open('configs/{app}/{env}/private.json'.format(app=application, env=environment)).read()
 
-@app.route("/testme/<regex('[a-Z]*'):application>-<regex('[a-Z]*'):environment>-<regex('[a-Z]*'):config>", methods=['GET'])
-def public_config(application, environment, config):
+from web import app
 
-	# return send_from_directory('configs/{app}/{env}'.format(app=application, env=environment.split('.')[0]), 'public.json', as_attachment=True) if '.json' in environment else open('configs/{app}/{env}/public.json'.format(app=application, env=environment)).read()
-	return '{"application":"'+application+'", "environment":"'+environment+'", "config":"'+config+'"}'
+
+@app.route("/config/<path:application>/<path:environment>", methods=['GET'])
+def public_config(application, environment):
+	return open('configs/{appname}/{env}/public.json'.format(appname=application, env=environment)).read()
 
 @app.route("/config/private/<path:application>/<path:environment>", methods=['GET'])
 def private_config(application, environment):
-
-	return send_from_directory('configs/{app}/{env}'.format(app=application, env=environment.split('.')[0]), 'private.json', as_attachment=True) if '.json' in environment else open('configs/{app}/{env}/private.json'.format(app=application, env=environment)).read()
-	
+	return open('configs/{appname}/{env}/private.json'.format(appname=application, env=environment)).read()
--- web.py
+++ web.py
@@ -3,13 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-class RegexConverter(BaseConverter):
-        def __init__(self, url_map, *items):
-                super(RegexConverter, self).__init__(url_map)
-                self.regex = items[0]
-
-app.url_map.converters['regex'] = RegexConverter
-
 from iconPCI import *
 from akamaiContent import *
 from janrain import *
